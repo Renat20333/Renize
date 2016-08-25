0fcb35f6a3f290b2e0525dec4bb11bf9aeddf438
Renat Kayumov <renat@gmail.com>
2016-08-25 14:17:22 +0500

    Мелькие правки

--- header.php
+++ header.php
@@ -2,9 +2,9 @@
 	<img id="header_logo" src="img/logo.jpg"><h1 class="header_logo_text">Renize</h1>
 	<nav>
 		<ul>
-		<li><a href="#">MAIN</a></li>
-		<li><a href="#">EMAIL</a></li>
-		<li><a href="#">Пункт меню</a></li>
+			<li><a href="#">MAIN</a></li>
+			<li><a href="#">EMAIL</a></li>
+			<li><a href="#">Пункт меню</a></li>
 		</ul>
 	</nav>
 	<h2 class="slogan">Сайт в разработке.</h2>
